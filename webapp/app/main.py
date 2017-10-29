1cb48d73e53ab8e3f0083e5d60ff01535dd51dbd
Eric Hth <eric_hong_2000@yahoo.fr>
2017-10-29 23:11:29 +0100

    WORKING

--- webapp/app/main.py
+++ webapp/app/main.py
@@ -12,11 +12,6 @@ def clean( data , selected_columns , selected_columns_data):
     data = data[selected_columns]
     data = data.drop_duplicates()
     data = data.dropna()
-    """ data.shape
-    (5043, 7)
-    (4920, 7)
-    (3789, 7)
-    """
     data.index.name = 'film_id'
     data = data.reset_index()
     info = data
@@ -76,84 +71,13 @@ def distance_matrix_( data_ ):
     data_ = addColumnForEachDirector(data_)
     data_ = preprocessing.scale(data_, with_mean=False )
     return distance_matrix(data_)
-def display(movie, recommendations ):
-    def print_( string ):
-        separator = "---------------------------"
-        print(separator + " " + string + " " + separator)
-        return
-    if movie is None or recommendations is None:
-        print_('Recommendation not available for ' + film_id)
-    else:
-        selected_columns_display = ['movie_title', 'genres','director_name','title_year']
-        print_("Selected Movie:")
-        print(movie[selected_columns_display].to_string(index=False,header=False))
-        print_("Recommendations:")
-        print(recommendations[selected_columns_display].to_string(index=False,header=False))
-def recommendations(movies , data_):
-    film_ids = [3,9,283,631,2607] # 3 The Dark Knight Rises - 9 Harry Potter - 283 Gladiator - 631 Twillight - 2607 The King's Speech
-    for film_id in film_ids:
-        selectedMovie, recommendations = getRecommendation_(movies,distanceMatrix,film_id)
-        display(selectedMovie, recommendations )
-    return
 
-def clustering_process( data_ ):
-    def addColumnForEachGenre( data ):
-        def getGenres( data ):
-            serie = data.genres.str.split('|')
-            serie_ = serie.agg(['sum'])
-            return list(set(serie_.values[0]))
-        def add_column_eachGenre( data, genres ):
-            for genre in genres:
-                data.loc[:,genre] = 0
-            return data
-        def add_column_genresSplit( data ):
-            genre_plit = data['genres'].apply(split_)
-            data.loc[:,'genres_split'] = genre_plit
-            return data
-        def fill_column_eachGenre( row ):
-            genres = row.genres_split
-            for genre in genres:
-                row[genre] = 1
-            return row
-        def split( string , separator):
-            return string.split(separator)
-        def split_( string):
-            return split(string,'|')
-        data_ = add_column_eachGenre(data,getGenres(data))
-        data_ = add_column_genresSplit(data)
-        data_ = data_.apply(fill_column_eachGenre, axis = 1)
-        data_ = data_.drop(['genres_split','genres'], axis=1)
-        return data_
-    def addColumnForEachDirector( data ):
-        data_ = get_dummies(data['director_name'])
-        data_ = pd.concat([data, data_], axis=1)
-        data_ = data_.drop(['director_name'], axis=1)
-        return data_;
-    data_ = addColumnForEachGenre(data_)
-    data_ = addColumnForEachDirector(data_)
-    data_ = preprocessing.scale(data_, with_mean=False )
-    return data_
-
-def clustering_plotSilhouette( data_ ):
-    start = 100
-    end = 1100
-    #end = 1100
-    range_ = [2] + range(start, end, 100)
-    res =[]
-    for k in range_:
-        kmeans = KMeans(n_clusters=k).fit(data_)
-        res.append(metrics.silhouette_score(data_,kmeans.labels_))
-    plt.plot(range_,res,marker='o')
-    plt.xlabel('Score de Silhouette')
-    plt.savefig('scoreSilhouette_{}_{}.png'.format(start,end))
-    plt.show()
-    return
 def init():
     data = pd.read_csv('data.csv', sep=",")
     global movies
     global distanceMatrix
     selected_columns_data = ['genres','duration','gross','director_name','budget','title_year']
-    selected_columns = selected_columns_data + ['movie_title']
+    selected_columns =  ['movie_title']
     movies, data_ = clean( data , selected_columns , selected_columns_data)
     distanceMatrix = distance_matrix_(data_)
     return
@@ -163,4 +87,4 @@ def getRecommendation(film_id ):
 def getRecommendation_display(selectedMovie,recommendations ):
     selectedMovie_ = selectedMovie.to_string(index=False,header=False)
     recommendations_ = recommendations.to_string(index=False,header=False)
-    return selectedMovie_ + '<div>' + recommendations_ '</div>'
+    return selectedMovie_ + '\n----------------------\n' + recommendations_
