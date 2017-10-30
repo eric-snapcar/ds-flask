03712d4e484a33e2aea503a7c654715064cf76f4
Eric Hth <eric_hong_2000@yahoo.fr>
2017-10-30 01:54:51 +0100

    test

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import Flask, render_template, request
 from .main import getRecommendation
 
+
+app = Flask(__name__)
+app.config.from_object('config')
+
 // @app.route()
 @app.route('/')
 def index():
@@ -12,7 +16,5 @@ def recommend():
     film_id = request.args.get('film_id') or 3
     return render_template('recommend.html', film_id=film_id, recommendation = getRecommendation(int(film_id)))
 
-app = Flask(__name__)
-app.config.from_object('config')
 if __name__ == "__main__":
     app.run()
