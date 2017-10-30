1a48744c60ed8e91dbc8a9463b96c15fe9c01741
Eric Hth <eric_hong_2000@yahoo.fr>
2017-10-30 01:49:58 +0100

    ok

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -1,9 +1,3 @@
 from flask import Flask
-from .views import app
-from . import models
 from . import main
 main.init()
-models.db.init_app(app)
-@app.cli.command()
-def test_command():
-    models.test_command()
--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,10 +1,8 @@
 from flask import Flask
 from flask import Flask, render_template, request
 from .main import getRecommendation
-# Init
-app = Flask(__name__)
-app.config.from_object('config')
-# App
+
+// @app.route()
 @app.route('/')
 def index():
     film_id = request.args.get('film_id') or 3
@@ -13,6 +11,8 @@ def index():
 def recommend():
     film_id = request.args.get('film_id') or 3
     return render_template('recommend.html', film_id=film_id, recommendation = getRecommendation(int(film_id)))
-# Main
+
+app = Flask(__name__)
+app.config.from_object('config')
 if __name__ == "__main__":
     app.run()
