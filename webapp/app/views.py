4ec99eb4cfe71938c4b74ad762bb181d76bade61
Eric Hth <eric@macbook-pro-de-eric.home>
2017-11-15 22:52:17 +0100

    main.py build_cache() # ... __init__.py views.py ...

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,10 +1,6 @@
 from flask import Flask
 from flask import Flask, render_template, request
 from .main import getRecommendation
-from .main_clustering import getRecommendation_ as getRecommendation_clustering
-from .main_dmatrix import getRecommendation__ as getRecommendation_distance
-from .main_clustering_v2 import getRecommendation as getRecommendation_clustering_v2
-from .main_dmatrix_v2 import getRecommendation as getRecommendation_distance_v2
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -18,12 +14,7 @@ def display_movie(movie,recommendations ):
         res += recommendations.to_html()
     return recommendations
 def render(film_id):
-    """
-    recommendation1 =  getRecommendation(int(film_id))
-    movie2, recommendations2 = getRecommendation_clustering_v2(int(film_id))
-    recommendation2 =   display(movie2, recommendations2 ) #getRecommendation_clustering(int(film_id))
-    """
-    movie, recommendations = getRecommendation_distance_v2(int(film_id))
+    movie, recommendations = getRecommendation(int(film_id))
     if movie is None or recommendations is None:
         return render_template('recommend.html',movie = None, recommendation = None, available = False)
     else :
