427905b127fc28fada181bf80fe663c8750a6367
Eric Hth <eric@snapcar.com>
2017-12-23 10:37:06 +0100

    TEST OK

--- webapp/app/main.py
+++ webapp/app/main.py
@@ -5,31 +5,52 @@ Created on Wed Nov 22 13:29:25 2017
 @author: ATruong1
 """
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 from scipy import sparse # Need this to create a sparse array
-from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+import ast
+import datetime
 #%%
 def charge():
     #chargement des données
-    data_ = pd.read_csv('data_flight_delay_2016.csv', sep=",",error_bad_lines=False)
-    list_para = pd.read_csv('list_para.csv', sep=",",error_bad_lines=False)
+    data_ = pd.read_csv('data_tronque_global.csv', sep=",",error_bad_lines=False)
+    coefs_ = pd.read_csv('coefs_global.csv', sep=",",error_bad_lines=False)
     data_ref = pd.read_csv('ref_airport.csv', sep=",",error_bad_lines=False)
+    dic_airport = pd.read_csv('dic_airport.csv', sep=",",error_bad_lines=False)
+    data_carrier = pd.read_csv('data_carrier.csv', sep=",",error_bad_lines=False)
+
+    coefs = coefs_['INTERCEPT_COEFS'][1:]
+    intercept = coefs_['INTERCEPT_COEFS'][0]
+    dic_airport = ast.literal_eval(dic_airport['ORIGIN_NUM'][0])
+    list_flight = data_['FLIGHT'].drop_duplicates().tolist()
 
     #préparation des variables
-    data_['FLIGHT'] = data_['ORIGIN'].astype('str') + '|' + data_['DEST'].astype('str') + '|' + data_['UNIQUE_CARRIER'].astype('str')
     scalingDF = data_[['DISTANCE', 'CRS_ELAPSED_TIME']].astype('float') # Numerical features
-    categDF = data_[['MONTH', 'DAY_OF_MONTH', 'FLIGHT', 'ARR_HOUR', 'DEP_HOUR']]
-    list_flight = data_['FLIGHT'].value_counts()
-    list_flight_filtered = list_flight[(list_flight>364)].index.values.tolist()
-    return data_, data_ref, list_para, list_flight_filtered, scalingDF, categDF
-def predict(origin, destination, carrier_code, date, hour_departure, hour_arrival, data_, data_ref, list_para, list_flight_filtered, scalingDF, categDF):
+    categDF = data_[['MONTH', 'DAY_OF_WEEK', 'ORIGIN_NUM',
+                    'DEST_NUM', 'ARR_HOUR', 'DEP_HOUR',
+                    'CARRIER_CODE']]
+    return data_, data_ref, coefs, intercept, list_flight, scalingDF, categDF, dic_airport, data_carrier
+
+def display(originCity,originCode,destCity,destCode,date,depHour,prediction):
+    return 'The flight from {} ({}) to {} ({}) on {} at {} will have a delay of {} min'.format(originCity,originCode,destCity,destCode,date,depHour,prediction)
+
+def predict(origin, destination, carrier_code, date, hour_departure, data_, data_ref, list_flight, dic_airport, scalingDF, categDF, coefs, intercept):
     flight = origin +'|'+destination+'|'+carrier_code
     day = int(date.split('/')[0])
     month = int(date.split('/')[1])
-    flightDF =[]
-    flightDF.insert(0, {'MONTH': month, 'DAY_OF_MONTH': day, 'FLIGHT': flight, 'ARR_HOUR': hour_arrival, 'DEP_HOUR': hour_departure})
-    if flight in list_flight_filtered:
-        categDF_flight = pd.concat([pd.DataFrame(flightDF),categDF[data_['FLIGHT'] == flight]], ignore_index = True).drop('FLIGHT', axis = 1)
+    day = datetime.datetime(2017, month, day)
+    day_of_week = day.weekday()+1
+
+    if flight in list_flight:
+        elapsed_time = data_[data_['FLIGHT'] == flight]['CRS_ELAPSED_TIME'].mean()
+        distance = data_[data_['FLIGHT'] == flight]['DISTANCE'].mean()
+        arrival_time = hour_departure + round(elapsed_time/60)
+        origin_num = dic_airport[origin]
+        dest_num = dic_airport[destination]
+        carrier = data_carrier[data_carrier['UNIQUE_CARRIER'] == carrier_code]['CARRIER_CODE'].values[0]
+        scale = pd.DataFrame([(distance, elapsed_time)], columns =['DISTANCE', 'CRS_ELAPSED_TIME'])
+        categ = pd.DataFrame([(month, day_of_week, origin_num, dest_num, arrival_time, hour_departure, carrier)], columns = ['MONTH', 'DAY_OF_WEEK', 'ORIGIN_NUM', 'DEST_NUM', 'ARR_HOUR', 'DEP_HOUR', 'CARRIER_CODE'])
     else:
         print('The flight does not exist, please check again your flight info:')
         print('     Origin and Destination Airport Code are written in a 3 letter capital format')
@@ -38,33 +59,30 @@ def predict(origin, destination, carrier_code, date, hour_departure, hour_arriva
         print('     Date is in the format DD/MM')
 
     encoder = OneHotEncoder() # Create encoder objec
-    categDF_encoded = encoder.fit_transform(categDF_flight)
-    x_topredict = sparse.csr_matrix(categDF_encoded.toarray()[0])
-    x_train = sparse.csr_matrix(categDF_encoded.toarray()[1:])
-    y_train = data_[data_['FLIGHT'] == flight]['ARR_DELAY'].values
-
-    l1_ratio_ = list_para[list_para['flight']==flight]['l1_ratio'].iloc[0]
-    alpha_ = list_para[list_para['flight']==flight]['alpha'].iloc[0]
-    SGD_model = linear_model.SGDRegressor(penalty='elasticnet', alpha = alpha_ ,l1_ratio = l1_ratio_)
-    SGD_model.fit(x_train, y_train)
-
-    y_predicted = SGD_model.predict(x_topredict)
-    return display(data_ref[data_ref['CODE']==origin].iloc[0]['CITY'],origin,data_ref[data_ref['CODE']==destination].iloc[0]['CITY'],destination,date,hour_departure,y_predicted[0])
-def display(originCity,originCode,destCity,destCode,date,depHour,prediction):
-    return 'The flight from {} ({}) to {} ({}) on {} at {} will have a delay of {} min'.format(originCity,originCode,destCity,destCode,date,depHour,prediction)
-
+    encoder.fit(categDF)
+    categDF_encoded = encoder.transform(categ)
+    scaler = StandardScaler() # create scaler object
+    scaler.fit(scalingDF)
+    scalingDF_sparse = sparse.csr_matrix(scaler.transform(scale)) # Transform the data and convert to sparse
+    x_to_predict = sparse.hstack((scalingDF_sparse, categDF_encoded))
+    y_predicted = x_to_predict.toarray().dot(coefs) + intercept
+    y_predicted = round(y_predicted[0])
+    return display(data_ref[data_ref['CODE']==origin].iloc[0]['CITY'],origin,data_ref[data_ref['CODE']==destination].iloc[0]['CITY'],destination,date,hour_departure,y_predicted)
+#%%
+def init():
+    global data_, data_ref, coefs, intercept, list_flight, scalingDF, categDF, dic_airport, data_carrier
+    data_, data_ref, coefs, intercept, list_flight, scalingDF, categDF, dic_airport, data_carrier = charge()
+    return
 
 
 #%%
-def init():
-    global data_, data_ref, list_para, list_flight_filtered, scalingDF, categDF
-    data_, data_ref, list_para, list_flight_filtered, scalingDF, categDF = charge()
-    return
-def getPrediction(origin, destination, carrier_code, date, hour_departure, hour_arrival):
-    return predict(origin, destination, carrier_code, date, hour_departure, hour_arrival, data_, data_ref, list_para, list_flight_filtered, scalingDF, categDF)
 def test():
-    return getPrediction("DFW", "DTW", "AA", "20/01", 11, 14)
+    return predict('ATL', 'BOS', 'DL', '12/12', 16, data_, data_ref, list_flight, dic_airport, scalingDF, categDF, coefs, intercept)
 
+
+#%%
+data_, data_ref, coefs, intercept, list_flight, scalingDF, categDF, dic_airport, data_carrier = charge()
+print test()
 """
    Unnamed: 0  MONTH  DAY_OF_MONTH  DAY_OF_WEEK UNIQUE_CARRIER ORIGIN DEST  \
 0           0      1             6            3             AA    DFW  DTW
